2885561b4c96ffd5242f9a2035a6915b535ae44d
rjazree <rjtazree@gmail.com>
2019-03-26 21:03:31 -0700

    commit

--- 31.php
+++ 31.php
@@ -2,9 +2,26 @@
 
 $stdin = fopen('php://stdin', 'r');
 $stdout = fopen('php://stdout', 'w');
-    //ini_set('precision', 19);
-    //phfscanf(STDIN,"%d%d\n",$n);
-    $s=trim(fgets(STDIN));
-    $a=explode(" ",$s);
-    $foo= ($a[0])/($a[1]);
-     echo round($foo, 25) ;
+   
+    fscanf(STDIN,"%d\n",$n);
+
+    for($i=1;$i<=$n;$i++){
+        $s=trim(fgets(STDIN));
+        $k=explode(" ",$s);
+        $a=$k[0];
+        $b=$k[1];
+        $c=$k[2];
+        $o=$a/$b;
+        echo (int)$o.".";;
+        $a=$a%$b;
+        $count=0;
+        while($count<$c){
+            $x=$a*10;
+            $a=$x%$b;
+            $x=$x/$b;
+            echo (int)$x;
+            $count++;
+
+        }
+        echo "\n";
+    }
